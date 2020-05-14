a23a04220f24654b44b1c9f7ff176b837d513b65
piyumi_atulugama <piyumi.atulugama@syscolabs.com>
2020-05-14 19:34:37 +0530

    modify the  files

--- src/test/java/com/sysco/api/sample/api/tests/GetEmployeeDetailsTest.java
+++ src/test/java/com/sysco/api/sample/api/tests/GetEmployeeDetailsTest.java
@@ -6,12 +6,9 @@ import com.sysco.api.sample.api.requests.functions.Employee;
 import com.sysco.api.sample.api.response.model.EmployeeModel;
 import com.sysco.api.sample.api.utils.ResponseUtil;
 import com.sysco.api.sample.api.types.*;
-import jdk.nashorn.internal.runtime.options.LoggingOption;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
-
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -30,7 +27,7 @@ public class GetEmployeeDetailsTest {
 
         Response employeeList = Employee.getEmployeeListDetials();
         EmployeeModel employeeModel = (EmployeeModel) ResponseUtil.getDataObjectValueInDataArray(employeeList.asString(), 0, EmployeeModel.class);
-        softAssert.assertEquals(ResponseUtil.getStatus(employeeList), StatusTestData.STATUS_SUCCESS, "Success code is appear as expected ");
+        softAssert.assertEquals(ResponseUtil.getStatus(employeeList), StatusTestData.STATUS_SUCCESS, "Sucess code is not apppear as expected");
         softAssert.assertEquals(employeeModel.employee_name, EmployeeTestData.EMP_NAME, "Employee details are not match as expected[name]");
         softAssert.assertEquals(employeeModel.employee_salary, EmployeeTestData.EMP_SALARY, "Employee details are not match as expected[salary]");
         softAssert.assertEquals(employeeModel.employee_age, EmployeeTestData.EMP_AGE, "Employee details are not match as expected[name]");
